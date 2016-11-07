748ccc977a5bfb11839dba16f3c5ed018af618f3
maks7 <maks757q@gmail.com>
2016-11-08 00:53:40 +0200

    fix migrate

--- migrations/m160916_136540_create_user_progress_images_fix.php
+++ migrations/m160916_136540_create_user_progress_images_fix.php
@@ -9,17 +9,17 @@ class m160916_136540_create_user_progress_images_fix extends Migration
 {
     public function up()
     {
-        $this->addColumn('user_progress_images', 'progress_key', $this->string(128));
-        $this->addColumn('user_progress_images', 'name', $this->string(100));
-        $this->addColumn('user_progress_images', 'short_description', $this->string(100));
-        $this->addColumn('user_progress_images', 'long_description', $this->string(255));
+//        $this->addColumn('user_progress_images', 'progress_key', $this->string(128));
+//        $this->addColumn('user_progress_images', 'name', $this->string(100));
+//        $this->addColumn('user_progress_images', 'short_description', $this->string(100));
+//        $this->addColumn('user_progress_images', 'long_description', $this->string(255));
 
         $progress = UserProgress::find()->all();
         /** @var $prog UserProgress */
         foreach ($progress as $prog){
             if(empty(UserProgressImages::find()->where(['name' => $prog->name])->one())){
                 /** @var $image UserProgressImages */
-                $image = $prog->image;
+                $image = $prog->data;
                 $image->name = $prog->name;
                 $image->progress_key = $prog->progress_key;
                 $image->short_description = $prog->short_description;
